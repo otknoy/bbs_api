9832aaa9219f9cc95c57200f54ff3d5dda4e18cc
Naoya Otsuka <otknoy@gmail.com>
2021-07-15 20:29:09 +0900

    graceful shutdown

--- main.go
+++ main.go
@@ -6,9 +6,13 @@ import (
 	"bbs_api/interfaces"
 	"bbs_api/openapi"
 	"bbs_api/service"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -36,7 +40,26 @@ func main() {
 	log.Println("server start")
 	defer log.Println("server stop")
 
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	done := make(chan struct{})
+	go func() {
+		<-ctx.Done()
+
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			log.Printf("shutdown error: %v\n", err)
+		}
+
+		close(done)
+	}()
+
 	if err := server.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatalln(err)
 	}
+
+	<-done
 }
