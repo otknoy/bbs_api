6d08d1f3c256269930ca7eae92969d814c15263b
Naoya Otsuka <otknoy@gmail.com>
2021-07-28 20:50:43 +0900

    add factory test

--- interfaces/response/factory_test.go
+++ interfaces/response/factory_test.go
@@ -2,10 +2,12 @@ package response_test
 
 import (
 	"bbs_api/domain/boardlist"
+	"bbs_api/domain/thread"
 	"bbs_api/domain/threadlist"
 	"bbs_api/interfaces/response"
 	"bbs_api/openapi"
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -66,4 +68,59 @@ func TestCreateThreadListResponse(t *testing.T) {
 	if diff := cmp.Diff(want, got); diff != "" {
 		t.Errorf("differs\n%v\n", diff)
 	}
+}
+
+func TestCreateThreadResponse(t *testing.T) {
+	in := &thread.Thread{
+		CommentList: []thread.Comment{
+			{
+				Meta: thread.Meta{
+					Number:   1,
+					UserName: "Kobe Bryant",
+					UserId:   "black_mamba",
+					PostedAt: time.Date(2020, 1, 2, 3, 4, 5, 6, time.UTC),
+				},
+				Text: "foobar",
+			},
+			{
+				Meta: thread.Meta{
+					Number:   2,
+					UserName: "Lebron James",
+					UserId:   "king_james",
+					PostedAt: time.Date(2020, 1, 2, 3, 4, 5, 7, time.UTC),
+				},
+				Text: "hoge",
+			},
+		},
+	}
+
+	want := &openapi.ThreadResponse{
+		CommentList: []openapi.Comment{
+			{
+				Meta: openapi.CommentMeta{
+					Number:   1,
+					UserName: "Kobe Bryant",
+					UserId:   "black_mamba",
+					PostedAt: time.Date(2020, 1, 2, 3, 4, 5, 6, time.UTC),
+				},
+				Text: "foobar",
+			},
+			{
+				Meta: openapi.CommentMeta{
+					Number:   2,
+					UserName: "Lebron James",
+					UserId:   "king_james",
+					PostedAt: time.Date(2020, 1, 2, 3, 4, 5, 7, time.UTC),
+				},
+				Text: "hoge",
+			},
+		},
+	}
+
+	got := response.CreateThreadResponse(in)
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("differs\n%v\n", diff)
+	}
+
 }
